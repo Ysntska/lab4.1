1d573d4e627dcc34a661bdaccb899ca35f7c44dd
Ysntska <yasynetska27@gmail.com>
2020-10-29 13:05:57 +0200

    first

---
+++ 4.1/4.1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double P;
+    double k, N;
+
+    cout << "N = "; cin >> N;
+    cout << endl;
+
+    k = N;
+    P = 1;
+    while (k <= 19)
+    {
+        P *= ((k - N) / (k + N) + 1);
+        k++;
+    }
+    cout << "P = " << P << endl;
+
+
+    k = N;
+    P = 1;
+    do
+    {
+        P *= ((k - N) / (k + N) + 1);
+        k++;
+    } while (k <= 19);
+    cout << "P = " << P << endl;
+
+
+    P = 1;
+    for (k = N; k <= 19; k++)
+    {
+        P *= ((k - N) / (k + N) + 1);
+    }
+    cout << "P = " << P << endl;
+
+    P = 1;
+    for (k = 19; k >= N; k--)
+    {
+        P *= ((k - N) / (k + N) + 1);
+    }
+    cout << "P = " << P << endl;
+
+
+    cin.get();
+    return 0;
+}
